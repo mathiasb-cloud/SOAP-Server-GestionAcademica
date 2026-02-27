a4d3a34dbc50daafd608173ebe50f608b631316d
Mathias Benites <i202406644@cibertec.edu.pe>
2026-02-26 19:14:53 -0500

    avisen para subirlo a AWS

--- REST-Alumno/src/main/java/cibertec/pe/repository/IAlumnoRepository.java
+++
@@ -1,6 +0,0 @@
-package cibertec.pe.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import cibertec.pe.model.Alumno;
-
-public interface IAlumnoRepository extends JpaRepository<Alumno, Integer> {
-}
